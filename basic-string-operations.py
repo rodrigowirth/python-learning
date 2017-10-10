787e170b1ef7c9292da02267790588106eb3f184
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-10-10 18:45:23 -0300

    Basic string operations

---
+++ basic-string-operations.py
@@ -0,0 +1,37 @@
+astring = "Hello world!"
+astring2 = 'Hello world!'
+
+astring = "Hello world!"
+print("single quotes are ' '")
+
+print(len(astring))
+
+astring = "Hello world!"
+print(astring.index("o"))
+
+astring = "Hello world!"
+print(astring.count("l"))
+
+astring = "Hello world!"
+print(astring[3:7])
+
+astring = "Hello world!"
+print(astring[3:7:2])
+
+astring = "Hello world!"
+print(astring[3:7])
+print(astring[3:7:1])
+
+astring = "Hello world!"
+print(astring[::-1])
+
+astring = "Hello world!"
+print(astring.upper())
+print(astring.lower())
+
+astring = "Hello world!"
+print(astring.startswith("Hello"))
+print(astring.endswith("asdfasdfasdf"))
+
+astring = "Hello world!"
+print(astring.split(" "))
